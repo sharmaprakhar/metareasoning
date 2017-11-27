b134ba318d5b2e30f23c17bd78d8802bcdbb9ec3
Justin Svegliato <justin.svegliato1@marist.edu>
2017-11-27 12:20:38 -0500

    Cleaned up experiment code

--- model-free-mlc.py
+++ model-free-mlc.py
@@ -1,59 +1,33 @@
-import numpy as np
-import random
-import matplotlib.pyplot as plt
 import itertools
-from multiprocessing import Process, Manager
-import time
-import utils
 import operator
-import tsp
-from sklearn import svm
-from sklearn import linear_model
+import random
+import time
+from multiprocessing import Manager, Process
+
 import matplotlib.pyplot as plt
+import numpy as np
 import pylab as pl
+from sklearn import linear_model, svm
 
-SOLUTION_QUALITY_CLASS_COUNT = 100
-SOLUTION_QUALITY_CLASS_BOUNDS = np.linspace(0, 1, SOLUTION_QUALITY_CLASS_COUNT)
-SOLUTION_QUALITY_CLASSES = range(SOLUTION_QUALITY_CLASS_COUNT)
+import tsp
+import utils
 
+QUALITY_CLASS_COUNT = 100
+QUALITY_CLASSES = range(QUALITY_CLASS_COUNT)
+QUALITY_CLASS_BOUNDS = np.linspace(0, 1, QUALITY_CLASS_COUNT)
 TIME_CLASSES = range(50)
 ACTIONS = [0, 1]
-
+SLEEP_INTERVAL = 0.5
 EPISODES = 200
 LEARNING_RATE = 0.01
 EPSILON = 0.1
-
-SLEEP_INTERVAL = 0.5
-
-
-def get_S():
-    return list(itertools.product(SOLUTION_QUALITY_CLASSES, TIME_CLASSES))
-
-
-def get_initial_Q_function():
-    return {s: {0: 0.1, 1: 0} for s in get_S()}
-    # return {s: {0: random.random(), 1: random.random()} for s in get_S()}
-
-
-def get_pi(Q):
-    return {s: max(Q[s].items(), key=operator.itemgetter(1))[0] for s in get_S()}
-
-
-def U(q, t):
-    # return 100 * q - np.exp((1 / 2) * t)
-    return 1000 * q
-
-
-def get_Q_values(Q, s):
-    return [Q[s][a] for a in Q[s]]
-
 
 def k_opt_tsp_solver(states, start_state, iterations, memory):
     tour = tsp.get_initial_random_tour(states, start_state)
     cities = tsp.get_swappable_cities(tour)
     distance = tsp.get_tour_distance(tour)
 
-    for t in range(iterations):
+    for _ in range(iterations):
         has_changed = False
 
         best_tour = tour
@@ -80,61 +54,65 @@ def k_opt_tsp_solver(states, start_state, iterations, memory):
     return tour
 
 
+def get_S():
+    return list(itertools.product(QUALITY_CLASSES, TIME_CLASSES))
+
+
+def get_initial_Q_function():
+    return {s: {0: random.random(), 1: random.random()} for s in get_S()}
+
+
+def get_pi(Q):
+    return {s: max(Q[s].items(), key=operator.itemgetter(1))[0] for s in get_S()}
+
+
+def U(q, t):
+    return 1000 * q
+
+
+def get_Q_values(Q, s):
+    return [Q[s][a] for a in Q[s]]
+
+
 def get_Q_function(episodes, learning_rate, epsilon):
-    print('Starting experiment with episodes = %f, learning_rate = %f, and epsilon = %f...' % (episodes, learning_rate, epsilon))
+    print('{"episodes": %f, "learning_rate": %f, "epsilon": %f}' % (episodes, learning_rate, epsilon))
 
-    # Create a list to keep track of the stopping points of each episode
-    stopping_points = []
+    values = []
 
-    # Generate a random Q-value function and its associated policy
     Q = get_initial_Q_function()
     pi = get_pi(Q)
 
-    # Learn the Q-value function for all episodes
     for episode in range(episodes):
-        print('Episode %d' % episode)
-
-        # Initialize the previous solution quality and computation time
-        previous_q = 0
+        start_q = 0
         start_t = time.time()
 
-        # Initialize the dictionary passed between the meta-level controller and the TSP solver
         memory = Manager().dict()
-        q = memory['q'] = previous_q
-        t = memory['t'] = start_t
+        memory['q'] = start_q
+        memory['t'] = start_t
 
-        # Initialize the current state and action
         q_class = 0
         t_class = 0
         s = (q_class, t_class)
-        a = memory['a'] = 1
+        a = 1
 
-        # Initialize a random instance of a k-TSP
         states = tsp.get_instance(50, 0, 10000, 1)
         start_state = list(states)[0]
 
-        # Calculate the heuristic to determine the solution quality at each time step
         heuristic = tsp.get_mst_distance(start_state, states)
 
-        # Start the TSP solver
         process = Process(target=k_opt_tsp_solver, args=(states, start_state, 1000, memory))
         process.start()
 
-        # Sleep for the sleep interval to let the anytime algorithm produce a solution
         time.sleep(SLEEP_INTERVAL)
 
-        # Repeat the following until the anytime algorithm has been interrupted
         while process.is_alive():
-            # Calculate the new solution quality and time
             next_q = heuristic / (memory['q'] + 0.00001)
             next_t = memory['t']
 
-            # Set up the next state
-            next_q_class = utils.digitize(next_q, SOLUTION_QUALITY_CLASS_BOUNDS)
+            next_q_class = utils.digitize(next_q, QUALITY_CLASS_BOUNDS)
             next_t_class = round((next_t - start_t) / SLEEP_INTERVAL)
             next_s = (next_q_class, next_t_class)
-     
-            # Update the Q-value function and the policy
+
             r = U(next_q_class, next_t_class) - U(q_class, t_class)
             next_Q_values = get_Q_values(Q, next_s)
             Q[s][a] += learning_rate * (r + max(next_Q_values) - Q[s][a])
@@ -142,28 +120,23 @@ def get_Q_function(episodes, learning_rate, epsilon):
 
             a = pi[next_s] if random.random() > epsilon else random.choice(ACTIONS)
 
-            # Update the current state
-            q = next_q
-            t = next_t
             q_class = next_q_class
             t_class = next_t_class
-            s = (next_q_class, next_t_class)
+            s = (q_class, t_class)
 
-            # Stop the anytime algorithm accordingly
             if a == 0:
                 process.terminate()
-                stopping_points.append(t_class)
+                values.append(t_class)
                 break
 
-            # Sleep to give the anytime algorithm time to compute more solutions
             time.sleep(SLEEP_INTERVAL)
 
-        print(np.mean(stopping_points[-20:]))
+        print('{"episode": %d, "value": %f}' % (episode, np.mean(values[-20:])))
 
-        f = open('experiment-%d-%f-%f.png' % (episodes, learning_rate, epsilon), 'w')
-        for stopping_point in stopping_points:
-            f.write('%d\n' % stopping_point)
-        f.close()
+    f = open('experiment-%d-%f-%f.png' % (episodes, learning_rate, epsilon), 'w')
+    for value in values:
+        f.write('%d\n' % value)
+    f.close()
 
     return Q
 
