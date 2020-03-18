577ae85335530ea9a79edfbcdfbcb915e73a0263
Justin Svegliato <justin.svegliato@gmail.com>
2020-03-18 17:13:53 -0400

    Added all results from the paper - will create a JS library for this

--- convergence_experiments.py
+++ convergence_experiments.py
@@ -81,33 +81,33 @@ def run_fourier_q_learning_experiments(params):
 def run():
     print("Convergence experiment [{}] with [alpha = {}, beta = {}, increment = {}]".format(PROBLEM_FILE, ALPHA, BETA, INCREMENT))
 
-    # tabular_sarsa_results = run_tabular_sarsa_experiments({
-    #     "alpha": 0.1,
-    #     "epsilon": 0.1,
-    #     "gamma": 1.0,
-    #     "decay": 0.999,
-    #     "episodes": 5000
-    # })
-    # print("Error: {} +/- {}".format(tabular_sarsa_results["mean_error"], tabular_sarsa_results["standard_deviation_error"]))
+    tabular_sarsa_results = run_tabular_sarsa_experiments({
+        "alpha": 0.1,
+        "epsilon": 0.1,
+        "gamma": 1.0,
+        "decay": 0.999,
+        "episodes": 500
+    })
+    print("Error: {} +/- {}".format(tabular_sarsa_results["mean_error"], tabular_sarsa_results["standard_deviation_error"]))
 
-    # tabular_q_learning_results = run_tabular_q_learning_experiments({
-    #     "alpha": 0.1,
-    #     "epsilon": 0.1,
-    #     "gamma": 1.0,
-    #     "decay": 0.999,
-    #     "episodes": 5000
-    # })
-    # print("Error: {} +/- {}".format(tabular_q_learning_results["mean_error"], tabular_q_learning_results["standard_deviation_error"]))
+    tabular_q_learning_results = run_tabular_q_learning_experiments({
+        "alpha": 0.1,
+        "epsilon": 0.1,
+        "gamma": 1.0,
+        "decay": 0.999,
+        "episodes": 500
+    })
+    print("Error: {} +/- {}".format(tabular_q_learning_results["mean_error"], tabular_q_learning_results["standard_deviation_error"]))
 
-    # fourier_sarsa_results = run_fourier_sarsa_experiments({
-    #     "alpha": 0.00001,
-    #     "epsilon": 0.1,
-    #     "order": 7,
-    #     "gamma": 1.0,
-    #     "decay": 0.999,
-    #     "episodes": 5000
-    # })
-    # print("Error: {} +/- {}".format(fourier_sarsa_results["mean_error"], fourier_sarsa_results["standard_deviation_error"]))
+    fourier_sarsa_results = run_fourier_sarsa_experiments({
+        "alpha": 0.00001,
+        "epsilon": 0.1,
+        "order": 7,
+        "gamma": 1.0,
+        "decay": 0.999,
+        "episodes": 500
+    })
+    print("Error: {} +/- {}".format(fourier_sarsa_results["mean_error"], fourier_sarsa_results["standard_deviation_error"]))
 
     fourier_q_learning_results = run_fourier_q_learning_experiments({
         "alpha": 0.00001,
@@ -115,7 +115,7 @@ def run():
         "order": 7,
         "gamma": 1.0,
         "decay": 0.999,
-        "episodes": 5000,
+        "episodes": 500,
         "checkpoint": 250
     })
     print("Error: {} +/- {}".format(fourier_q_learning_results["mean_error"], fourier_q_learning_results["standard_deviation_error"]))
@@ -132,14 +132,12 @@ def run():
     axis.spines["top"].set_visible(False)
     axis.spines["right"].set_visible(False)
 
-    # p1 = plt.plot(range(len(tabular_sarsa_results["smoothed_errors"])), tabular_sarsa_results["smoothed_errors"], color="r")
-    # p2 = plt.plot(range(len(fourier_sarsa_results["smoothed_errors"])), fourier_sarsa_results["smoothed_errors"], color="b")
-    # p3 = plt.plot(range(len(tabular_q_learning_results["smoothed_errors"])), tabular_q_learning_results["smoothed_errors"], color="g")
+    p1 = plt.plot(range(len(tabular_sarsa_results["smoothed_errors"])), tabular_sarsa_results["smoothed_errors"], color="r")
+    p2 = plt.plot(range(len(fourier_sarsa_results["smoothed_errors"])), fourier_sarsa_results["smoothed_errors"], color="b")
+    p3 = plt.plot(range(len(tabular_q_learning_results["smoothed_errors"])), tabular_q_learning_results["smoothed_errors"], color="g")
     p4 = plt.plot(range(len(fourier_q_learning_results["smoothed_errors"])), fourier_q_learning_results["smoothed_errors"], color="y")
 
-    # plt.legend((p1[0], p2[0], p3[0], p4[0]), ("SARSA(Table)", "SARSA(Fourier)", "Q-learning(Table)", "Q-learning(Fourier)"), loc=1, ncol=2)
-    # plt.legend((p1[0], p3[0], ), ("SARSA(Table)", "Q-learning(Table)"), loc=1, ncol=2)
-    plt.legend((p4[0], ), ("Q-learning(Fourier)", ), loc=1, ncol=2)
+    plt.legend((p1[0], p2[0], p3[0], p4[0]), ("SARSA(Table)", "SARSA(Fourier)", "Q-learning(Table)", "Q-learning(Fourier)"), loc=1, ncol=2)
 
     plt.tight_layout()
     plt.show()
@@ -158,28 +156,28 @@ def plot():
     axis.spines["top"].set_visible(False)
     axis.spines["right"].set_visible(False)
 
-    filename = PLOTS_DIRECTORY + "{}-learning-curve.pdf".format(NAME)
-    # tabular_sarsa_statistics = utils.load(RESULTS_DIRECTORY + "tabular-sarsa-[0.1]-[0.1]-{}.json".format(NAME))
-    # tabular_q_learning_statistics = utils.load(RESULTS_DIRECTORY + "tabular-q-[0.1]-[0.1]-{}.json".format(NAME))
-    # fourier_sarsa_statistics = utils.load(RESULTS_DIRECTORY + "fourier-sarsa-[1e-05]-[0.1]-[3]-{}.json".format(NAME))
-    fourier_q_learning_statistics = utils.load(RESULTS_DIRECTORY + "fourier-q-[1e-05]-[0.1]-[3]-{}.json".format(NAME))
+    # filename = PLOTS_DIRECTORY + "{}-learning-curve.pdf".format(NAME)
+    tabular_sarsa_statistics = utils.load(RESULTS_DIRECTORY + "tabular-sarsa-[0.1]-[0.1]-{}.json".format(NAME))
+    tabular_q_learning_statistics = utils.load(RESULTS_DIRECTORY + "tabular-q-[0.1]-[0.1]-{}.json".format(NAME))
+    fourier_sarsa_statistics = utils.load(RESULTS_DIRECTORY + "fourier-sarsa-[1e-05]-[0.1]-[7]-{}.json".format(NAME))
+    fourier_q_learning_statistics = utils.load(RESULTS_DIRECTORY + "fourier-q-[1e-05]-[0.1]-[7]-{}.json".format(NAME))
 
-    # tabular_sarsa_utitilities = utils.get_smoothed_values(tabular_sarsa_statistics["utilities"], PLOT_WINDOW_SIZE)
-    # fourier_sarsa_utilities = utils.get_smoothed_values(fourier_sarsa_statistics["utilities"], PLOT_WINDOW_SIZE)
-    # tabular_q_learning_utilities = utils.get_smoothed_values(tabular_q_learning_statistics["utilities"], PLOT_WINDOW_SIZE)
+    tabular_sarsa_utitilities = utils.get_smoothed_values(tabular_sarsa_statistics["utilities"], PLOT_WINDOW_SIZE)
+    fourier_sarsa_utilities = utils.get_smoothed_values(fourier_sarsa_statistics["utilities"], PLOT_WINDOW_SIZE)
+    tabular_q_learning_utilities = utils.get_smoothed_values(tabular_q_learning_statistics["utilities"], PLOT_WINDOW_SIZE)
     fourier_q_learning_utilities = utils.get_smoothed_values(fourier_q_learning_statistics["utilities"], PLOT_WINDOW_SIZE)
 
-    # p1 = plt.plot(range(len(tabular_sarsa_utitilities)), tabular_sarsa_utitilities, color="khaki")
-    # p2 = plt.plot(range(len(tabular_q_learning_utilities)), tabular_q_learning_utilities, color="seagreen")
-    # p3 = plt.plot(range(len(fourier_sarsa_utilities)), fourier_sarsa_utilities, color="indianred")
+    p1 = plt.plot(range(len(tabular_sarsa_utitilities)), tabular_sarsa_utitilities, color="khaki")
+    p2 = plt.plot(range(len(tabular_q_learning_utilities)), tabular_q_learning_utilities, color="seagreen")
+    p3 = plt.plot(range(len(fourier_sarsa_utilities)), fourier_sarsa_utilities, color="indianred")
     p4 = plt.plot(range(len(fourier_q_learning_utilities)), fourier_q_learning_utilities, color="steelblue")
 
-    # plt.legend((p1[0], p2[0], p3[0], p4[0]), ("SARSA(Table)", "Q-learning(Table)", "SARSA(Fourier)", "Q-learning(Fourier)"), loc=4, fontsize="small")
-    plt.legend((p4[0], ), ("Q-learning(Fourier)", ), loc=4, fontsize="small")
+    plt.legend((p1[0], p2[0], p3[0], p4[0]), ("SARSA(Table)", "Q-learning(Table)", "SARSA(Fourier)", "Q-learning(Fourier)"), loc=4, fontsize="small")
+    # plt.legend((p4[0], ), ("Q-learning(Fourier)", ), loc=4, fontsize="small")
 
     plt.tight_layout()
 
-    figure.savefig(filename, bbox_inches="tight")
+    figure.savefig('test.pdf', bbox_inches="tight")
 
 
 def test():
@@ -219,7 +217,7 @@ def test():
 
 
 def main():
-    test()
+    plot()
 
 
 if __name__ == "__main__":
